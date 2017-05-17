cec582d21fb944c54f25808cc91b527053791336
mpiniarski <piniarski.m@gmail.com>
2017-05-17 23:25:23 +0200

    Split gradle projects

--- backend/core/src/main/java/com/joma/studies/article/dto/exception/ArticleBuildingException.java
+++ backend/core/src/main/java/com/joma/studies/article/dto/exception/ArticleBuildingException.java
@@ -1,7 +1,7 @@
 package com.joma.studies.article.dto.exception;
 
 public class ArticleBuildingException extends Exception {
-    private static final String MESSAGE = "Unable to build article";
+    private static final String MESSAGE = "Unable to build com.joma.studies.article.dto.article";
 
     public ArticleBuildingException() {
         super(MESSAGE);
