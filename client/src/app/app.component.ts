1c11c8eba7939c6bf37be59b11bb5d1737bc4bd1
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2017-05-25 00:55:13 +0200

    Initial web page layout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+    private appName: string = "MedSort";
+    private query: string;
+    private starred = [false, false];
+    private articles = [
+        {
+            "title": "Super article",
+            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit non velit non condimentum. Duis convallis feugiat gravida. Aenean in luctus turpis. Suspendisse mollis cursus pellentesque. Integer id tortor posuere, ornare diam at, dictum nunc. In lacinia eros vitae augue consectetur aliquam. Sed malesuada erat at purus scelerisque, in sollicitudin orci ullamcorper."
+        },
+        {
+            "title": "Awesome article",
+            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit non velit non condimentum. Duis convallis feugiat gravida. Aenean in luctus turpis. Suspendisse mollis cursus pellentesque. Integer id tortor posuere, ornare diam at, dictum nunc. In lacinia eros vitae augue consectetur aliquam. Sed malesuada erat at purus scelerisque, in sollicitudin orci ullamcorper."
+        },
+        {
+            "title": "The greatest article of all time with the best title in the world",
+            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit non velit non condimentum. Duis convallis feugiat gravida. Aenean in luctus turpis. Suspendisse mollis cursus pellentesque. Integer id tortor posuere, ornare diam at, dictum nunc. In lacinia eros vitae augue consectetur aliquam. Sed malesuada erat at purus scelerisque, in sollicitudin orci ullamcorper."
+        }
+    ]
+
+    search() {
+        console.log(this.query);
+    }
+
+    rateArticle(index) {
+        this.starred[index] === true ? this.starred[index] = false : this.starred[index] = true;
+    }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MdlModule } from 'angular2-mdl';
 
 import { AppComponent } from './app.component';
+import { EllipsisPipe } from './ellipsis.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        EllipsisPipe
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        MdlModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
