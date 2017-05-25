6b824734c8103d2502d227f379d4aecdf8602b88
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2017-05-25 19:27:27 +0200

    Expandable articles

--- client/src/app/article/article.component.ts
+++ client/src/app/article/article.component.ts
@@ -8,24 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class ArticleComponent implements OnInit {
 
     private starred = [false, false];
+    private expand = [false, false];
     private articles = [
         {
             "title": "Super article",
-            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit non velit non condimentum. Duis convallis feugiat gravida. Aenean in luctus turpis. Suspendisse mollis cursus pellentesque. Integer id tortor posuere, ornare diam at, dictum nunc. In lacinia eros vitae augue consectetur aliquam. Sed malesuada erat at purus scelerisque, in sollicitudin orci ullamcorper."
+            "date": "14.11.2014",
+            "author": "Stephen King",
+            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur urna odio, elementum quis tellus in, euismod pellentesque massa. Sed et ultricies felis. Aenean eleifend velit urna, ut laoreet nunc tempor nec. In vitae auctor odio, ut ornare enim. Nullam euismod ante elit, sed fermentum tortor sagittis eget. Aenean sed nunc orci. Donec sagittis nibh in nibh consectetur, at hendrerit ex sagittis. Sed et mauris sed lectus scelerisque tempus. Suspendisse dui purus, accumsan sed ligula id, dignissim convallis ipsum. Nulla in lacus vel nisl molestie luctus. Pellentesque aliquet sapien ac nunc tristique, vulputate accumsan felis eleifend.\n\nMaecenas auctor, elit et laoreet pellentesque, dui erat sagittis arcu, nec lobortis quam urna et magna. Pellentesque quis mauris id ligula consectetur pellentesque ut et mauris. Donec eu tincidunt urna, ut ultrices mi. Proin finibus vitae massa eget elementum. Cras vitae lacus at augue rutrum posuere sed eu augue. Pellentesque bibendum bibendum nibh, eu vestibulum odio auctor eleifend. Nam feugiat magna non est tincidunt blandit."
         },
         {
             "title": "Awesome article",
-            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit non velit non condimentum. Duis convallis feugiat gravida. Aenean in luctus turpis. Suspendisse mollis cursus pellentesque. Integer id tortor posuere, ornare diam at, dictum nunc. In lacinia eros vitae augue consectetur aliquam. Sed malesuada erat at purus scelerisque, in sollicitudin orci ullamcorper."
+            "date": "14.11.2014",
+            "author": "Stephen King",
+            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur urna odio, elementum quis tellus in, euismod pellentesque massa. Sed et ultricies felis. Aenean eleifend velit urna, ut laoreet nunc tempor nec. In vitae auctor odio, ut ornare enim. Nullam euismod ante elit, sed fermentum tortor sagittis eget. Aenean sed nunc orci. Donec sagittis nibh in nibh consectetur, at hendrerit ex sagittis. Sed et mauris sed lectus scelerisque tempus. Suspendisse dui purus, accumsan sed ligula id, dignissim convallis ipsum. Nulla in lacus vel nisl molestie luctus. Pellentesque aliquet sapien ac nunc tristique, vulputate accumsan felis eleifend."
         },
         {
             "title": "The greatest article of all time with the best title in the world",
-            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit non velit non condimentum. Duis convallis feugiat gravida. Aenean in luctus turpis. Suspendisse mollis cursus pellentesque. Integer id tortor posuere, ornare diam at, dictum nunc. In lacinia eros vitae augue consectetur aliquam. Sed malesuada erat at purus scelerisque, in sollicitudin orci ullamcorper."
+            "date": "14.11.2014",
+            "author": "Stephen King",
+            "abstract": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur urna odio, elementum quis tellus in, euismod pellentesque massa. Sed et ultricies felis. Aenean eleifend velit urna, ut laoreet nunc tempor nec. In vitae auctor odio, ut ornare enim. Nullam euismod ante elit, sed fermentum tortor sagittis eget. Aenean sed nunc orci. Donec sagittis nibh in nibh consectetur, at hendrerit ex sagittis. Sed et mauris sed lectus scelerisque tempus. Suspendisse dui purus, accumsan sed ligula id, dignissim convallis ipsum. Nulla in lacus vel nisl molestie luctus. Pellentesque aliquet sapien ac nunc tristique, vulputate accumsan felis eleifend."
         }
     ]
 
     constructor() { }
 
     ngOnInit() { }
+
+    expandArticle(index) {
+        this.expand[index] === true ? this.expand[index] = false : this.expand[index] = true;
+    }
 
     rateArticle(index) {
         this.starred[index] === true ? this.starred[index] = false : this.starred[index] = true;
